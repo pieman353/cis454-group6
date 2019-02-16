4ab009e0252153ca32f5f4b36804e0fa9f7c31ad
pieman353 <pieman353@aol.com>
2019-02-16 05:16:32 -0500

    Add class to handle database credentials
    Change databaseInfo.php with your own database credentials

---
+++ php/databaseInfo.php
@@ -0,0 +1,16 @@
+<?php
+//Change this information with your database credentials
+class databaseInfo {
+    static public $product_database_name = "prod";
+    static public $product_database_table = "products";
+    static public $product_database_username = "root";
+    static public $product_database_password = "root";
+
+    static public $signin_database_name = "signup";
+    static public $signin_database_table = "acc";
+    static public $signin_database_username = "root";
+    static public $signin_database_password = "";
+
+    static public $database_host = "localhost";
+}
+?>
--- php/zhuce.php
+++ php/zhuce.php
@@ -11,17 +11,18 @@
             $lastname = filter_input(INPUT_POST,'lastname');
             if(!empty($username)){
                 if(!empty($password)){
-                    $host = "localhost";
-                    $dbusername = "root";
-                    $dbpassword = "";
-                    $dbname = "aSignup";
+                    //$host = "localhost";
+                    //$dbusername = "root";
+                    //$dbpassword = "root";
+                    //$dbname = "signup";
 
-                    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+                    $conn = new mysqli($database_host, $signin_database_username, 
+                    $signin_database_password, $signin_database_name);
 
                     if(mysqli_connect_error()){
                         die('Connect Error ('.mysqli_connect_errno().')'.mysqli_connect_error());
                     }else{
-                        $sql = "INSERT INTO acc (firstname,lastname,username, password) values ('$firstname','$lastname','$username','$password')";
+                        $sql = "INSERT INTO $signin_database_table (firstname,lastname,username, password) values ('$firstname','$lastname','$username','$password')";
                         if($conn->query($sql)){
                             echo "<div class='cc'>Success</div>";
                             echo '<a href="../FrontPage.html">Go back</a>';
